b275ce4c2410b1023b31346b59d89e9aea7eadfd
Shade Alabsa <shade@Drizzt.local>
2013-04-10 16:55:16 -0400

    Initial Commit

---
+++ ResSim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ResSim
+//
+//  Created by Shade Alabsa on 4/10/13.
+//  Copyright (c) 2013 Shade Alabsa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
